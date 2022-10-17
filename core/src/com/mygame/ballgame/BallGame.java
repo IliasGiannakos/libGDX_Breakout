123a81ffd3333edce8f291eb9d8385771099afa7
ilias giannakos <ilgiannakos@gmail.com>
2022-10-17 09:05:45 +0300

    first ball render

--- core/src/com/mygame/ballgame/BallGame.java
+++ core/src/com/mygame/ballgame/BallGame.java
@@ -3,29 +3,23 @@ package com.mygame.ballgame;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class BallGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	ShapeRenderer shape;
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		shape = new ShapeRenderer();
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void render() {
+		shape.begin(ShapeRenderer.ShapeType.Filled);
+		shape.circle(50,50,50);
+		shape.setColor(1,0.7f,0,1);
+		shape.end();
 	}
 }
