dd6897f93ef6ca1b1e9720bf7218c0548cfcf2b6
Ilias Giannakos <ilgiannakos@gmail.com>
2022-10-17 10:57:43 +0300

    Ball class implementation

--- core/src/com/mygame/ballgame/Ball.java
+++ core/src/com/mygame/ballgame/Ball.java
@@ -1,4 +1,38 @@
 package com.mygame.ballgame;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
 public class Ball {
+
+    int x;
+    int y;
+    int size;
+    int xSpeed;
+    int ySpeed;
+
+//    ball obj constructor
+    public Ball(int x, int y, int size, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public void update() {
+        x += xSpeed;
+        y += ySpeed;
+        if (x -size < 0 || x + size > Gdx.graphics.getWidth()) {
+            xSpeed = -xSpeed;
+        }
+        if (y - size < 0 || y + size > Gdx.graphics.getHeight()) {
+            ySpeed = -ySpeed;
+        }
+    }
+
+    public void draw(ShapeRenderer shape) {
+        shape.circle(x, y, size);
+    }
+
 }
