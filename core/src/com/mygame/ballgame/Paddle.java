1ebbaf41171561073b050b8b001e7131b10d06b4
Ilias Giannakos <ilgiannakos@gmail.com>
2022-10-20 09:36:31 +0300

    paddle implementation

--- core/src/com/mygame/ballgame/Paddle.java
+++ core/src/com/mygame/ballgame/Paddle.java
@@ -1,4 +1,33 @@
 package com.mygame.ballgame;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
 public class Paddle {
+
+    int x;
+    int y;
+    int width;
+    int height;
+
+    public Paddle(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void update() {
+        x = Gdx.input.getX();
+        if (x - width / 2 < 0) {
+            x = x-width/2;
+        }
+        if (x+width/2 > Gdx.graphics.getWidth()) {
+            x = x+width/2;
+        }
+    }
+
+    public void draw(ShapeRenderer shape) {
+        shape.rect(x, y, width, height);
+    }
 }
