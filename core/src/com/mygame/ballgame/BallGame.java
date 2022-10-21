fcdded6da4093f4a7cb7ca2b32755accaceae5e8
Ilias Giannakos <ilgiannakos@gmail.com>
2022-10-21 09:13:06 +0300

    add ballHitbox & ball-paddle collision

--- core/src/com/mygame/ballgame/BallGame.java
+++ core/src/com/mygame/ballgame/BallGame.java
@@ -13,10 +13,11 @@ public class BallGame extends ApplicationAdapter {
 
 
 
+
     @Override
     public void create() {
         shape = new ShapeRenderer();
-        ball = new Ball(250, 150, 15, 5, 5);
+        ball = new Ball(300, 250, 15, 0, 0);
         paddle = new Paddle(0, 10, 70, 10);
     }
 
@@ -24,6 +25,7 @@ public class BallGame extends ApplicationAdapter {
     public void render() {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         shape.begin(ShapeRenderer.ShapeType.Filled);
+        ball.checkCollision(paddle);
         ball.update();
         ball.draw(shape);
         paddle.draw(shape);
